50e24c97047271a436d344ca90d042a2078d9c7c
adiY666 <adi.yekutieljr@gmail.com>
2025-01-14 11:09:47 +0200

    created Gripper.java

---
+++ src/main/java/frc/robot/subsystems/Gripper.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
+
+public class Gripper extends MotoredGenericSubsystem {
+
+    private static final double PULL_SPEED = -0.5;
+    private static final double PUSH_SPEED = 0.5;
+
+    public Gripper(String namespaceName, SparkMax motor) {
+        super(namespaceName, motor);
+    }
+
+    public void intake() {
+        apply(PULL_SPEED);
+    }
+
+    public void release() {
+        apply(PUSH_SPEED);
+    }
+
+    public boolean hasCoral() {
+        return false;
+    }
+
+    @Override
+    public void configureDashboard() {
+    }
+}

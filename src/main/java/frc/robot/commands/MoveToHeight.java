b880b7d70b364ca1964e8a71d9f6d2eeecd679e9
adiY666 <adi.yekutieljr@gmail.com>
2025-01-14 20:47:02 +0200

    created the command MoveToHeight.java

---
+++ src/main/java/frc/robot/commands/MoveToHeight.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import com.spikes2212.command.genericsubsystem.commands.smartmotorcontrollergenericsubsystem.
+        MoveSmartMotorControllerGenericSubsystem;
+import com.spikes2212.control.FeedForwardSettings;
+import com.spikes2212.control.PIDSettings;
+import com.spikes2212.util.UnifiedControlMode;
+import frc.robot.subsystems.Elevator;
+
+import java.util.function.Supplier;
+
+public class MoveToHeight extends MoveSmartMotorControllerGenericSubsystem {
+
+    private final Elevator elevator;
+
+    public MoveToHeight(Elevator elevator, PIDSettings pidSettings, FeedForwardSettings feedForwardSettings,
+                        UnifiedControlMode controlMode, Supplier<Double> setpoint) {
+        super(elevator, pidSettings, feedForwardSettings, controlMode, setpoint);
+        this.elevator = elevator;
+    }
+}

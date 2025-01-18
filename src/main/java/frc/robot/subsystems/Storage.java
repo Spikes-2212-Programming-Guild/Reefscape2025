54dfe2f3fc99cbc0d96d3bc635453b4014d89112
adiY666 <adi.yekutieljr@gmail.com>
2025-01-18 11:49:10 +0200

    renamed INTAKE_SPEED and RELEASE_SPEED

--- src/main/java/frc/robot/subsystems/Storage.java
+++ src/main/java/frc/robot/subsystems/Storage.java
@@ -7,19 +7,19 @@ import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.Spa
 
 public class Storage extends MotoredGenericSubsystem {
 
-    private static final double PULL_SPEED = -0.5;
-    private static final double PUSH_SPEED = 0.5;
+    private static final double INTAKE_SPEED = -0.5;
+    private static final double RELEASE_SPEED = 0.5;
 
     public Storage(String namespaceName, SparkMax motor) {
         super(namespaceName, motor);
     }
 
     public void intake() {
-        apply(PULL_SPEED);
+        apply(INTAKE_SPEED);
     }
 
     public void release() {
-        apply(PUSH_SPEED);
+        apply(RELEASE_SPEED);
     }
 
     public boolean hasCoral() {

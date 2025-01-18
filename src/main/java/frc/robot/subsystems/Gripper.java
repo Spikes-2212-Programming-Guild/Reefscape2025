784cadd6e47293f84abc6e9496c5ec5ae0d25c3d
adiY666 <adi.yekutieljr@gmail.com>
2025-01-18 17:28:23 +0200

    added limit in "configureDashboard"

--- src/main/java/frc/robot/subsystems/Gripper.java
+++ src/main/java/frc/robot/subsystems/Gripper.java
@@ -2,7 +2,10 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
 import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
+import com.spikes2212.dashboard.RootNamespace;
 import edu.wpi.first.wpilibj.DigitalInput;
+
+import java.util.function.Supplier;
 
 public class Gripper extends MotoredGenericSubsystem {
 
@@ -19,5 +22,6 @@ public class Gripper extends MotoredGenericSubsystem {
 
     @Override
     public void configureDashboard() {
+        namespace.putBoolean("limit", limit::get);
     }
 }

c12100314ccf4efb0b8832b2fc21ec54db246bb1
adiY666 <adi.yekutieljr@gmail.com>
2025-01-18 18:11:42 +0200

    put the constant in the dashboard

--- src/main/java/frc/robot/commands/IntakeCoral.java
+++ src/main/java/frc/robot/commands/IntakeCoral.java
@@ -1,13 +1,17 @@
 package frc.robot.commands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import com.spikes2212.dashboard.RootNamespace;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Storage;
 
+import java.util.function.Supplier;
+
 public class IntakeCoral extends MoveGenericSubsystem {
 
-    private static final double INTAKE_SPEED = -0.5;
+    private static final RootNamespace namespace = new RootNamespace("take corral");
+    private static final Supplier<Double> INTAKE_SPEED = namespace.addConstantDouble("intake speed", -0.5);
     private final Storage storage;
 
     public IntakeCoral(Storage storage) {

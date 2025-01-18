0cf1b0a8aa133f2142159a3eb63b79abb95a2785
adiY666 <adi.yekutieljr@gmail.com>
2025-01-18 18:09:30 +0200

    fixed isFinished in ReleaseCoral.java

--- src/main/java/frc/robot/commands/ReleaseCoral.java
+++ src/main/java/frc/robot/commands/ReleaseCoral.java
@@ -1,10 +1,8 @@
 package frc.robot.commands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
-import com.spikes2212.dashboard.Namespace;
 import com.spikes2212.dashboard.RootNamespace;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Storage;
 
 import java.util.function.Supplier;
@@ -15,19 +13,24 @@ public class ReleaseCoral extends MoveGenericSubsystem {
     private static final Supplier<Double> TIME_TO_RELEASE = namespace.addConstantDouble("time to release", 0.5);
     private static final Supplier<Double> RELEASE_SPEED = namespace.addConstantDouble("release speed", 0.5);
     private final Storage storage;
-    private double lastTimeInStorage;
+
+    private double startTime;
 
     public ReleaseCoral(Storage storage) {
         super(storage, RELEASE_SPEED);
         this.storage = storage;
-        this.lastTimeInStorage = 0;
+    }
+
+    @Override
+    public void initialize() {
+        startTime = Timer.getFPGATimestamp();
     }
 
     @Override
     public boolean isFinished() {
         if (storage.hasCoral()) {
-            lastTimeInStorage = Timer.getFPGATimestamp();
+            startTime = Timer.getFPGATimestamp();
         }
-        return Timer.getFPGATimestamp() - lastTimeInStorage >= TIME_TO_RELEASE;
+        return Timer.getFPGATimestamp() - startTime >= TIME_TO_RELEASE.get();
     }
 }

5365d23c68cc2e98e81b19feac1fa6797a73a543
adiY666 <adi.yekutieljr@gmail.com>
2025-01-18 18:03:59 +0200

    put the constant in the dashboard

--- src/main/java/frc/robot/commands/ReleaseCoral.java
+++ src/main/java/frc/robot/commands/ReleaseCoral.java
@@ -1,14 +1,19 @@
 package frc.robot.commands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import com.spikes2212.dashboard.Namespace;
+import com.spikes2212.dashboard.RootNamespace;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Storage;
 
+import java.util.function.Supplier;
+
 public class ReleaseCoral extends MoveGenericSubsystem {
 
-    private static final double TIME_TO_RELEASE = 0.5;
-    private static final double RELEASE_SPEED = 0.5;
+    private static final RootNamespace namespace = new RootNamespace("release corral");
+    private static final Supplier<Double> TIME_TO_RELEASE = namespace.addConstantDouble("time to release", 0.5);
+    private static final Supplier<Double> RELEASE_SPEED = namespace.addConstantDouble("release speed", 0.5);
     private final Storage storage;
     private double lastTimeInStorage;
 

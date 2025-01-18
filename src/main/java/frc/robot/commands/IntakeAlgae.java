8ef2d007f0db9ddba4750cffd62eda706055ea77
adiY666 <adi.yekutieljr@gmail.com>
2025-01-18 17:18:37 +0200

    put the constant in the dashboard

--- src/main/java/frc/robot/commands/IntakeAlgae.java
+++ src/main/java/frc/robot/commands/IntakeAlgae.java
@@ -1,11 +1,15 @@
 package frc.robot.commands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import com.spikes2212.dashboard.RootNamespace;
 import frc.robot.subsystems.Gripper;
+
+import java.util.function.Supplier;
 
 public class IntakeAlgae extends MoveGenericSubsystem {
 
-    private static final double INTAKE_SPEED = -0.5;
+    private static final RootNamespace namespace = new RootNamespace("intake algae");
+    private static final Supplier<Double> INTAKE_SPEED = namespace.addConstantDouble("intake speed", 0);
 
     private final Gripper gripper;
 

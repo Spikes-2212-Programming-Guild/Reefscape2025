62e04c86f6258d983d5b52a2ad329104ccfd7676
GIL <eingar.gil@gmail.com>
2025-01-19 20:24:00 +0200

    renamed ABSOLUTE_POSITION to ABSOLUTE_POSITION_DISCONTINUITY_POINT

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -35,7 +35,7 @@ public class SwerveModule extends DashboardedSubsystem {
     private static final double WHEEL_DIAMETER = 4 * 0.0254;
     private static final double DEGREES_IN_ROTATIONS = 360;
     private static final double SECONDS_IN_MINUTE = 60;
-    private static final double ABSOLUTE_POSITION = 1;
+    private static final double ABSOLUTE_POSITION_DISCONTINUITY_POINT = 1;
 
     private static final double MAX_DISTANCE_TO_ROTATE = 90;
     private static final double DEGREES_TO_FLIP = 180;
@@ -117,7 +117,7 @@ public class SwerveModule extends DashboardedSubsystem {
 
     public void configureAbsoluteEncoder() {
         MagnetSensorConfigs magnetConfigs = new MagnetSensorConfigs()
-                .withAbsoluteSensorDiscontinuityPoint(ABSOLUTE_POSITION)
+                .withAbsoluteSensorDiscontinuityPoint(ABSOLUTE_POSITION_DISCONTINUITY_POINT)
                 .withSensorDirection(cancoderInverted ? SensorDirectionValue.Clockwise_Positive :
                         SensorDirectionValue.CounterClockwise_Positive).withMagnetOffset(offset);
         absoluteEncoder.getConfigurator().apply(magnetConfigs);

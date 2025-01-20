ad643e92e3b34d3dd3dd2d043e8fc1e60f1071c6
GIL <eingar.gil@gmail.com>
2025-01-20 16:47:55 +0200

    made Translation2d fields private moved gyro ahead of SwerveModules removes this from kinematics

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -13,24 +13,25 @@ public class Drivetrain extends DashboardedSubsystem {
     private static final double TRACK_WIDTH = -1;
     private static final double TRACK_LENGTH = -1;
 
-    public static final Translation2d CENTER_OF_ROBOT = new Translation2d(0, 0);
-    public static final Translation2d FRONT_LEFT_WHEEL_POSITION =
+    private static final Translation2d CENTER_OF_ROBOT = new Translation2d(0, 0);
+    private static final Translation2d FRONT_LEFT_WHEEL_POSITION =
             new Translation2d(TRACK_WIDTH / 2, TRACK_LENGTH / 2);
-    public static final Translation2d FRONT_RIGHT_WHEEL_POSITION =
+    private static final Translation2d FRONT_RIGHT_WHEEL_POSITION =
             new Translation2d(TRACK_WIDTH / 2, -TRACK_LENGTH / 2);
-    public static final Translation2d BACK_LEFT_WHEEL_POSITION =
+    private static final Translation2d BACK_LEFT_WHEEL_POSITION =
             new Translation2d(-TRACK_WIDTH / 2, TRACK_LENGTH / 2);
-    public static final Translation2d BACK_RIGHT_WHEEL_POSITION =
+    private static final Translation2d BACK_RIGHT_WHEEL_POSITION =
             new Translation2d(-TRACK_WIDTH / 2, -TRACK_LENGTH / 2);
 
     private static final String NAMESPACE_NAME = "drivetrain";
+
+    private final AHRS gyro;
 
     private final SwerveModule frontLeft;
     private final SwerveModule frontRight;
     private final SwerveModule backLeft;
     private final SwerveModule backRight;
     private final SwerveDriveKinematics kinematics;
-    private final AHRS gyro;
 
     public Drivetrain(SwerveModule frontLeft, SwerveModule frontRight, SwerveModule backLeft,
                       SwerveModule backRight, AHRS gyro) {
@@ -39,7 +40,7 @@ public class Drivetrain extends DashboardedSubsystem {
         this.frontRight = frontRight;
         this.backLeft = backLeft;
         this.backRight = backRight;
-        this.kinematics = new SwerveDriveKinematics(FRONT_LEFT_WHEEL_POSITION,
+        kinematics = new SwerveDriveKinematics(FRONT_LEFT_WHEEL_POSITION,
                 FRONT_RIGHT_WHEEL_POSITION, BACK_LEFT_WHEEL_POSITION, BACK_RIGHT_WHEEL_POSITION);
         this.gyro = gyro;
     }

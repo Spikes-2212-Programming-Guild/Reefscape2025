0502a9f429b34173dd75f4b7d8cd06cf2309525d
adiY666 <adi.yekutieljr@gmail.com>
2025-01-21 21:23:01 +0200

    killed gripper

--- src/main/java/frc/robot/commands/IntakeAlgae.java
+++ src/main/java/frc/robot/commands/IntakeAlgae.java
@@ -10,10 +10,7 @@ public class IntakeAlgae extends MoveGenericSubsystem {
     private static final RootNamespace NAMESPACE = new RootNamespace("intake algae");
     private static final Supplier<Double> INTAKE_SPEED = NAMESPACE.addConstantDouble("intake speed", -0.5);
 
-    private final Gripper gripper;
-
     public IntakeAlgae(Gripper gripper) {
         super(gripper, INTAKE_SPEED);
-        this.gripper = gripper;
     }
 }

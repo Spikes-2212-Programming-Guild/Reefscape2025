e60313f47b8adcf8f3a159402b3d905748e41316
adiY666 <adi.yekutieljr@gmail.com>
2025-01-21 20:44:58 +0200

    made intake coral as the name

--- src/main/java/frc/robot/commands/IntakeCoral.java
+++ src/main/java/frc/robot/commands/IntakeCoral.java
@@ -8,7 +8,7 @@ import java.util.function.Supplier;
 
 public class IntakeCoral extends MoveGenericSubsystem {
 
-    private static final RootNamespace NAMESPACE = new RootNamespace("take coral");
+    private static final RootNamespace NAMESPACE = new RootNamespace("intake coral");
     private static final Supplier<Double> INTAKE_SPEED = NAMESPACE.addConstantDouble("intake speed", -0.5);
 
     public IntakeCoral(Storage storage) {

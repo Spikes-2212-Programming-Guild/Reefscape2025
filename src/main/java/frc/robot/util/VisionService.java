78870a06c012088fee011cd7453387b823419424
adiY666 <adi.yekutieljr@gmail.com>
2025-01-21 19:30:57 +0200

    created limelight that exist

---
+++ src/main/java/frc/robot/util/VisionService.java
@@ -0,0 +1,13 @@
+package frc.robot.util;
+
+import com.spikes2212.dashboard.RootNamespace;
+import com.spikes2212.util.Limelight;
+
+public class VisionService {
+
+    private final Limelight limelight;
+
+    public VisionService(Limelight limelight) {
+        this.limelight = limelight;
+    }
+}

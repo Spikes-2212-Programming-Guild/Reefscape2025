a65b7f62f100ae7690bb3872190ca1fcc5bd2a58
adiY666 <adi.yekutieljr@gmail.com>
2025-01-21 18:02:19 +0200

    created new command that move the elevator for a specific heights (the reef levels)

---
+++ src/main/java/frc/robot/commands/MoveToLevel.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+import com.spikes2212.command.genericsubsystem.commands.smartmotorcontrollergenericsubsystem.MoveSmartMotorControllerGenericSubsystem;
+import com.spikes2212.control.FeedForwardSettings;
+import com.spikes2212.control.PIDSettings;
+import com.spikes2212.util.UnifiedControlMode;
+import frc.robot.subsystems.Elevator;
+
+import java.util.function.Supplier;
+
+public class MoveToLevel extends MoveSmartMotorControllerGenericSubsystem {
+
+    private final Elevator elevator;
+
+    public MoveToLevel(Elevator elevator, PIDSettings pidSettings, FeedForwardSettings feedForwardSettings,
+                        Elevator.ReefLevel level) {
+        super(elevator, pidSettings, feedForwardSettings, UnifiedControlMode.POSITION, () -> level.height);
+        this.elevator = elevator;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return super.isFinished() || elevator.canMove(elevator.getSpeed());
+    }
+}

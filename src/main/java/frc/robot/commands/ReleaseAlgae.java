7d290fac3ba66622a77a29ec1043c2959ec55ce9
adiY666 <adi.yekutieljr@gmail.com>
2025-01-22 12:14:43 +0200

    killed gripper

--- src/main/java/frc/robot/commands/ReleaseAlgae.java
+++ src/main/java/frc/robot/commands/ReleaseAlgae.java
@@ -12,13 +12,10 @@ public class ReleaseAlgae extends MoveGenericSubsystem {
     private static final Supplier<Double> TIME_TO_RELEASE = NAMESPACE.addConstantDouble("time to release", 0.5);
     private static final Supplier<Double> RELEASE_SPEED = NAMESPACE.addConstantDouble("release speed", 0.5);
 
-    private final Gripper gripper;
-
     private double startTime;
 
     public ReleaseAlgae(Gripper gripper) {
         super(gripper, RELEASE_SPEED);
-        this.gripper = gripper;
     }
 
     @Override

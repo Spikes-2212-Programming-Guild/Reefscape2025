3bde60c01665f457d2307e5c1aaba5307037b1c4
adiY666 <adi.yekutieljr@gmail.com>
2025-01-24 02:15:50 +0200

    commited fo commit

--- src/main/java/frc/robot/subsystems/ElevatorPositionManagement.java
+++ src/main/java/frc/robot/subsystems/ElevatorPositionManagement.java
@@ -1,16 +1,17 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class ElevatorPositionManagement {
 
-    private final int lastLevel;
+    private final SparkMax elevator;
 
-    public ElevatorPositionManagement(int lastLevel) {
-        this.lastLevel = lastLevel;
+    public ElevatorPositionManagement(SparkMax elevator) {
+        this.elevator = elevator;
     }
 
-    public int level(double speed, int lastLevel) {
+    public int getLevel(double speed, int lastLevel) {
         if (speed > 0) {
             return lastLevel + 1;
         }
@@ -20,7 +21,6 @@ public class ElevatorPositionManagement {
         return lastLevel;
     }
 
-    public void changeLevel() {
-
+    public void changeLevel(double speed, int wantedHeight) {
     }
 }

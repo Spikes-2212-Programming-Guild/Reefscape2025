ca19730b40a06a6edd262a52d2bb946e86f3cbc4
adiY666 <adi.yekutieljr@gmail.com>
2025-01-26 20:06:13 +0200

    commited for commit

--- src/main/java/frc/robot/subsystems/CoralJoint.java
+++ src/main/java/frc/robot/subsystems/CoralJoint.java
@@ -1,4 +1,21 @@
 package frc.robot.subsystems;
 
-public class CoralJoint {
+import com.revrobotics.spark.SparkMax;
+import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.SparkGenericSubsystem;
+
+public class CoralJoint  extends SparkGenericSubsystem {
+
+    public enum STORAGE_POSE {
+        L1(-1), L2(-1), L3(-1), L4(-1);
+
+        public final double degree;
+
+        STORAGE_POSE(double degree) {
+            this.degree = degree;
+        }
+    }
+
+    public CoralJoint(String namespaceName, SparkMax joint) {
+        super(namespaceName, joint);
+    }
 }

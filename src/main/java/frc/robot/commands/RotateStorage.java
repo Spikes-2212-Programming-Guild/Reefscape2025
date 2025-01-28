daa1860c64e40b5747655b740edb1fb8148c4f97
adiY666 <adi.yekutieljr@gmail.com>
2025-01-28 18:57:44 +0200

    changed sparkMAX to SparkWrapper

--- src/main/java/frc/robot/commands/RotateStorage.java
+++ src/main/java/frc/robot/commands/RotateStorage.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 
-import com.spikes2212.command.genericsubsystem.commands.smartmotorcontrollergenericsubsystem.
-        MoveSmartMotorControllerGenericSubsystem;
+import com.spikes2212.command.genericsubsystem.commands.smartmotorcontrollergenericsubsystem.MoveSmartMotorControllerGenericSubsystem;
 import com.spikes2212.control.FeedForwardSettings;
 import com.spikes2212.control.PIDSettings;
 import com.spikes2212.util.UnifiedControlMode;
@@ -9,13 +8,13 @@ import frc.robot.subsystems.CoralJoint;
 
 public class RotateStorage extends MoveSmartMotorControllerGenericSubsystem {
 
-    private final CoralJoint coralJoint;
     private static final double JOINT_SPEED = 0.3;
+    private final CoralJoint coralJoint;
 
     public RotateStorage(CoralJoint coralJoint, PIDSettings pidSettings, FeedForwardSettings feedForwardSettings,
                          CoralJoint.STORAGE_POSE pose) {
         super(coralJoint, pidSettings, feedForwardSettings, UnifiedControlMode.POSITION,
-                ()-> pose.neededPitch, true);
+                () -> pose.neededPitch, true);
         this.coralJoint = coralJoint;
     }
 
--- src/main/java/frc/robot/subsystems/CoralJoint.java
+++ src/main/java/frc/robot/subsystems/CoralJoint.java
@@ -3,9 +3,10 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.SmartMotorControllerGenericSubsystem;
 import com.spikes2212.util.smartmotorcontrollers.SmartMotorController;
+import com.spikes2212.util.smartmotorcontrollers.SparkWrapper;
 import edu.wpi.first.wpilibj.DigitalInput;
 
-public class CoralJoint  extends SmartMotorControllerGenericSubsystem {
+public class CoralJoint extends SmartMotorControllerGenericSubsystem {
 
     public enum STORAGE_POSE {
         L1(-1), L2(-1), L3(-1), L4(-1);
@@ -20,13 +21,10 @@ public class CoralJoint  extends SmartMotorControllerGenericSubsystem {
     private final DigitalInput minLimit;
     private final DigitalInput maxLimit;
 
-    private final SparkMax joint;
-
-    public CoralJoint(String namespaceName, DigitalInput minLimit, DigitalInput maxLimit, SparkMax joint) {
-        super(namespaceName, (SmartMotorController) joint);
+    public CoralJoint(String namespaceName, DigitalInput minLimit, DigitalInput maxLimit, SparkWrapper joint) {
+        super(namespaceName, joint);
         this.minLimit = minLimit;
         this.maxLimit = maxLimit;
-        this.joint = joint;
     }
 
     public boolean canMove(double speed) {

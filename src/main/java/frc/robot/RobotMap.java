9488b06c591e28c15e92fbca7b289df490b8c73a
adiY666 <adi.yekutieljr@gmail.com>
2025-01-28 18:28:21 +0200

    made Gripper.java a singleton class

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,11 +3,11 @@ package frc.robot;
 public class RobotMap {
 
     public interface CAN {
-
+        int GRIPPER = -1;
     }
     
     public interface DIO {
-
+        int LIMIT = -1;
     }
     
     public interface PWM {
--- src/main/java/frc/robot/subsystems/Gripper.java
+++ src/main/java/frc/robot/subsystems/Gripper.java
@@ -1,12 +1,23 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkLowLevel;
 import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
 import com.spikes2212.util.smartmotorcontrollers.SparkWrapper;
 import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.RobotMap;
 
 public class Gripper extends MotoredGenericSubsystem {
 
     private final DigitalInput limit;
+
+    private static Gripper instance;
+    public static Gripper getInstance() {
+        if (instance == null) {
+            instance = new Gripper("gripper", SparkWrapper.createSparkMax(RobotMap.CAN.GRIPPER,
+                    SparkLowLevel.MotorType.kBrushless), new DigitalInput(RobotMap.DIO.LIMIT));
+        }
+        return instance;
+    }
 
     public Gripper(String namespaceName, SparkWrapper motor, DigitalInput limit) {
         super(namespaceName, motor);

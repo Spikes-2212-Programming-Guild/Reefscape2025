451b11468679430c8bcc8bc296a7475c94a11bcb
adiY666 <adi.yekutieljr@gmail.com>
2025-01-28 15:29:24 +0200

    changed to SparkWrapper instead

--- src/main/java/frc/robot/subsystems/Gripper.java
+++ src/main/java/frc/robot/subsystems/Gripper.java
@@ -1,14 +1,14 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
 import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
+import com.spikes2212.util.smartmotorcontrollers.SparkWrapper;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Gripper extends MotoredGenericSubsystem {
 
     private final DigitalInput limit;
 
-    public Gripper(String namespaceName, SparkMax motor, DigitalInput limit) {
+    public Gripper(String namespaceName, SparkWrapper motor, DigitalInput limit) {
         super(namespaceName, motor);
         this.limit = limit;
     }

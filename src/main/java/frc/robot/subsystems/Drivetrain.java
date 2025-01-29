9599b6c2038beb4239c8299d01d512cc7d3e546b
GIL <eingar.gil@gmail.com>
2025-01-29 18:52:31 +0200

    deleted extra line

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -91,7 +91,6 @@ public class Drivetrain extends DashboardedSubsystem {
         backRight.resetRelativeEncoder();
     }
 
-
     @Override
     public void configureDashboard() {
         namespace.putNumber("gyro", gyro::getAngle);

a790d169fd46252e286e96e62aa8db927ab4c70d
GIL <eingar.gil@gmail.com>
2025-01-30 20:07:56 +0200

    no real reason, not even done but damn is it fun to press ctrl k

--- src/main/java/frc/robot/subsystems/AlgaeJoint.java
+++ src/main/java/frc/robot/subsystems/AlgaeJoint.java
@@ -1,17 +1,16 @@
 package frc.robot.subsystems;
 
 import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.SmartMotorControllerGenericSubsystem;
-import com.spikes2212.util.smartmotorcontrollers.SmartMotorController;
 import com.spikes2212.util.smartmotorcontrollers.SparkWrapper;
 
 public class AlgaeJoint extends SmartMotorControllerGenericSubsystem {
 
     private static final String NAMESPACE_NAME = "storage";
 
-    private final SparkWrapper joint;
+    private final SparkWrapper spark;
 
-    public AlgaeJoint(SparkWrapper joint) {
-        super(NAMESPACE_NAME, joint);
-        this.joint = joint;
+    public AlgaeJoint(SparkWrapper spark) {
+        super(NAMESPACE_NAME, spark);
+        this.spark = spark;
     }
 }

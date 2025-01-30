9ff00ed7e84fec3ad097bc5522612f3996b94c93
GIL <eingar.gil@gmail.com>
2025-01-30 20:11:14 +0200

    deleted unnecessary imports

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,7 +6,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.SerialPort;
 
 public class Drivetrain extends DashboardedSubsystem {
 

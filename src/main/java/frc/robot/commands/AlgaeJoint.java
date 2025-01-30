ca02e9c5b3efa1498061a7614bf43995ef4dcf53
GIL <eingar.gil@gmail.com>
2025-01-30 20:26:28 +0200

    committing just because

--- src/main/java/frc/robot/commands/AlgaeJoint.java
+++ src/main/java/frc/robot/commands/AlgaeJoint.java
@@ -3,4 +3,10 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class AlgaeJoint extends Command {
+
+
+    @Override
+    public void execute() {
+        super.execute();
+    }
 }
--- src/main/java/frc/robot/subsystems/AlgaeJoint.java
+++ src/main/java/frc/robot/subsystems/AlgaeJoint.java
@@ -2,15 +2,22 @@ package frc.robot.subsystems;
 
 import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.SmartMotorControllerGenericSubsystem;
 import com.spikes2212.util.smartmotorcontrollers.SparkWrapper;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class AlgaeJoint extends SmartMotorControllerGenericSubsystem {
 
     private static final String NAMESPACE_NAME = "storage";
 
-    private final SparkWrapper spark;
+    private final DigitalInput topLimit;
+    private final DigitalInput bottomLimit;
 
-    public AlgaeJoint(SparkWrapper spark) {
+    public AlgaeJoint(SparkWrapper spark, DigitalInput topLimit, DigitalInput bottomLimit) {
         super(NAMESPACE_NAME, spark);
-        this.spark = spark;
+        this.topLimit = topLimit;
+        this.bottomLimit = bottomLimit;
+    }
+
+    public boolean canMove(double speeds){
+        return (speeds > 0 && topLimit.get())||(speeds < 0 && bottomLimit.get());
     }
 }

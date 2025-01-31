a6f9e08cb47e11c562f1c71c63eaf51090350490
GIL <eingar.gil@gmail.com>
2025-01-31 19:17:54 +0200

    made RotateAlgaeJointToTop.java

---
+++ src/main/java/frc/robot/commands/RotateAlgaeJointToTop.java
@@ -0,0 +1,17 @@
+package frc.robot.commands;
+
+import com.spikes2212.command.genericsubsystem.GenericSubsystem;
+import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import com.spikes2212.dashboard.RootNamespace;
+
+import java.util.function.Supplier;
+
+public class RotateAlgaeJointToTop extends MoveGenericSubsystem {
+
+    private static final RootNamespace NAMESPACE = new RootNamespace("rotate algae joint to top");
+    private static final Supplier<Double> INTAKE_SPEED = NAMESPACE.addConstantDouble("intake speed", 0.5);
+
+    public RotateAlgaeJointToTop(GenericSubsystem subsystem) {
+        super(subsystem, INTAKE_SPEED);
+    }
+}

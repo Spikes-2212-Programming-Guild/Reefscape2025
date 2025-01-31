010249cf4afc4bfb844e7b4102280c46d89079c0
GIL <eingar.gil@gmail.com>
2025-01-31 18:46:13 +0200

    updated CAN ports

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -6,18 +6,18 @@ public class RobotMap {
 
         int FRONT_LEFT_DRIVE_TALON_FX = 1;
         int FRONT_RIGHT_DRIVE_TALON_FX = 4;
-        int BACK_LEFT_DRIVE_TALON_FX = -1;
-        int BACK_RIGHT_DRIVE_TALON_FX = -1;
+        int BACK_LEFT_DRIVE_TALON_FX = 7;
+        int BACK_RIGHT_DRIVE_TALON_FX = 10;
 
         int FRONT_LEFT_TURN_SPARK_MAX = 2;
-        int FRONT_RIGHT_TURN_SPARK_MAX = -1;
-        int BACK_LEFT_TURN_SPARK_MAX = -1;
-        int BACK_RIGHT_TURN_SPARK_MAX = -1;
+        int FRONT_RIGHT_TURN_SPARK_MAX = 5;
+        int BACK_LEFT_TURN_SPARK_MAX = 8;
+        int BACK_RIGHT_TURN_SPARK_MAX = 11;
 
         int FRONT_LEFT_ABSOLUTE_ENCODER = 3;
-        int FRONT_RIGHT_ABSOLUTE_ENCODER = -1;
-        int BACK_LEFT_ABSOLUTE_ENCODER = -1;
-        int BACK_RIGHT_ABSOLUTE_ENCODER = -1;
+        int FRONT_RIGHT_ABSOLUTE_ENCODER = 6;
+        int BACK_LEFT_ABSOLUTE_ENCODER = 9;
+        int BACK_RIGHT_ABSOLUTE_ENCODER = 12;
     }
     
     public interface DIO {

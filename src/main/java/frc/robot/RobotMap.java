d70f94186d14cfc5bfc5e983ee47778a0b0a3b27
GIL <eingar.gil@gmail.com>
2025-01-31 18:37:39 +0200

    not done yet

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,17 +4,17 @@ public class RobotMap {
 
     public interface CAN {
 
-        int FRONT_LEFT_DRIVE_TALON_FX = -1;
-        int FRONT_RIGHT_DRIVE_TALON_FX = -1;
+        int FRONT_LEFT_DRIVE_TALON_FX = 1;
+        int FRONT_RIGHT_DRIVE_TALON_FX = 4;
         int BACK_LEFT_DRIVE_TALON_FX = -1;
         int BACK_RIGHT_DRIVE_TALON_FX = -1;
 
-        int FRONT_LEFT_TURN_SPARK_MAX = -1;
+        int FRONT_LEFT_TURN_SPARK_MAX = 2;
         int FRONT_RIGHT_TURN_SPARK_MAX = -1;
         int BACK_LEFT_TURN_SPARK_MAX = -1;
         int BACK_RIGHT_TURN_SPARK_MAX = -1;
 
-        int FRONT_LEFT_ABSOLUTE_ENCODER = -1;
+        int FRONT_LEFT_ABSOLUTE_ENCODER = 3;
         int FRONT_RIGHT_ABSOLUTE_ENCODER = -1;
         int BACK_LEFT_ABSOLUTE_ENCODER = -1;
         int BACK_RIGHT_ABSOLUTE_ENCODER = -1;

a509bf38aecdc97ddec9cd6162b91d0fa9445eee
adiY666 <adi.yekutieljr@gmail.com>
2025-02-06 15:04:49 +0200

    i didn't do nothing

--- src/main/java/frc/robot/commands/IntakeAlgae.java
+++ src/main/java/frc/robot/commands/IntakeAlgae.java
@@ -9,8 +9,8 @@ import java.util.function.Supplier;
 public class IntakeAlgae extends MoveGenericSubsystem {
 
     private static final RootNamespace NAMESPACE = new RootNamespace("intake algae");
-    private static final Supplier<Double> INTAKE_SPEED = NAMESPACE.addConstantDouble("intake speed", 0.2);
-    private static final Supplier<Double> TIME_TO_INTAKE = NAMESPACE.addConstantDouble("time to intake", 0.5);
+    private static final Supplier<Double> INTAKE_SPEED = NAMESPACE.addConstantDouble("intake speed", 0.9);
+    private static final Supplier<Double> TIME_TO_INTAKE = NAMESPACE.addConstantDouble("time to intake", 0.2);
 
     private double startTime;
 

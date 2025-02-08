5f30791e4304810f1ec14f650ec6e66cb87bfc3f
adiY666 <adi.yekutieljr@gmail.com>
2025-02-08 12:10:12 +0200

    saved Robot.java and RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,14 +4,14 @@ public class RobotMap {
 
     public interface CAN {
 
-        int ELEVATOR_MASTER_SPARK = -1;
-        int ELEVATOR_SLAVE_SPARK = -1;
+        int ELEVATOR_MASTER_SPARK = 10;
+        int ELEVATOR_SLAVE_SPARK = 1;
     }
     
     public interface DIO {
 
-        int ELEVATOR_TOP_LIMIT = -1;
-        int ELEVATOR_BOTTOM_LIMIT = -1;
+        int ELEVATOR_TOP_LIMIT = 9;
+        int ELEVATOR_BOTTOM_LIMIT = 8;
     }
     
     public interface PWM {

531f9ecb81b11e221c545f4dee58814b6c74739d
GIL <eingar.gil@gmail.com>
2025-02-08 14:52:34 +0200

    ctr alt l

--- src/main/java/frc/robot/commands/autonomous/PPAuto.java
+++ src/main/java/frc/robot/commands/autonomous/PPAuto.java
@@ -1,9 +1,8 @@
 package frc.robot.commands.autonomous;
 
-import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
-public class PPAuto extends PathPlannerAuto{
+public class PPAuto extends PathPlannerAuto {
 
     public PPAuto(String autoName) {
         super(autoName);

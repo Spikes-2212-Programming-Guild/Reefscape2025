6638b7ebfe55a4850aba0802f54692f0a2d58b58
TurtleMinecraft <schnitzelgirl69@gmail.com>
2025-02-11 13:50:57 +0200

    added some test code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,18 +4,36 @@
 
 package frc.robot;
 
+import com.spikes2212.dashboard.RootNamespace;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.*;
+import frc.robot.subsystems.AlgaeJoint;
+import frc.robot.subsystems.CoralJoint;
+import frc.robot.subsystems.Storage;
+
+import java.util.function.Supplier;
 
 public class Robot extends TimedRobot {
 
+    private static final RootNamespace namespace = new RootNamespace("robot");
+    private Storage storage;
+    private AlgaeJoint algaeJoint;
+
     @Override
     public void robotInit() {
+        storage = Storage.getInstance();
+        algaeJoint = AlgaeJoint.getInstance();
+        namespace.putCommand("intake coral", new IntakeCoral(storage));
+        namespace.putCommand("release coral", new ReleaseCoral(storage));
+        namespace.putCommand("rotate algae top", new RotateAlgaeJointToTop(algaeJoint));
+        namespace.putCommand("rotate algae bottom", new RotateAlgaeJointToBottom(algaeJoint));
     }
 
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
+        namespace.update();
     }
 
     @Override

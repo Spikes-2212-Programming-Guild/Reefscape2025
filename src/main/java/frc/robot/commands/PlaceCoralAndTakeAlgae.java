2485f3c79fed67d2da3b1f59256fce985a0116c0
GIL <eingar.gil@gmail.com>
2025-02-13 21:50:13 +0200

    command groups not clear

--- src/main/java/frc/robot/commands/PlaceCoralAndTakeAlgae.java
+++ src/main/java/frc/robot/commands/PlaceCoralAndTakeAlgae.java
@@ -18,11 +18,15 @@ public class PlaceCoralAndTakeAlgae extends SequentialCommandGroup {
                         new RotateAlgaeJointToBottom(algaeJoint)),
                 new ParallelCommandGroup(
                         new ReleaseCoral(storage), new IntakeAlgae(gripper)),
-                new ConditionalCommand(new SequentialCommandGroup(new ParallelCommandGroup(
-                        new Drive(drivetrain, () -> speed, () -> 0.0, () -> 0.0, false,
-                                false, false).withTimeout(0.5),
-                        new RotateStorage(coralJoint, CoralJoint.StoragePose.RESTING)),
-                        new MoveToHeight(elevator, Elevator.ElevatorLevel.PROCESSOR)),
+                new ConditionalCommand(
+                        new SequentialCommandGroup(
+                                new ParallelCommandGroup(
+                                        new Drive(drivetrain, () -> speed, () -> 0.0, () -> 0.0, false, false,
+                                                false).withTimeout(0.5),
+                                        new RotateStorage(coralJoint, CoralJoint.StoragePose.RESTING)
+                                ),
+                                new MoveToHeight(elevator, Elevator.ElevatorLevel.PROCESSOR)
+                        ),
                         new ParallelCommandGroup(
                                 new RotateAlgaeJointToTop(algaeJoint),
                                 new RotateStorage(coralJoint, CoralJoint.StoragePose.INTAKE),

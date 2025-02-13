73144aef9da393bd7ea1a9cea0301d7e9db36d56
adiY666 <adi.yekutieljr@gmail.com>
2025-02-13 14:21:46 +0200

    changed PickAlgaeFloar.java to PickAlgae.java

---
+++ src/main/java/frc/robot/commands/PickAlgae.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.AlgaeJoint;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Gripper;
+
+public class PickAlgae extends SequentialCommandGroup {
+
+    public PickAlgae(Elevator elevator, AlgaeJoint algaeJoint, Gripper gripper) {
+        addCommands(new RotateAlgaeJointToBottom(algaeJoint), new IntakeAlgae(gripper));
+    }
+}

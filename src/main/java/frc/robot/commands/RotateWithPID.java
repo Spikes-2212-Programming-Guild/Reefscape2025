f63bac0c18439abf8c8e0ecc90de33045ccfe38b
GIL <eingar.gil@gmail.com>
2025-02-13 21:21:50 +0200

    added RotateWithPID.java

---
+++ src/main/java/frc/robot/commands/RotateWithPID.java
@@ -0,0 +1,53 @@
+package frc.robot.commands;
+
+import com.spikes2212.control.FeedForwardController;
+import com.spikes2212.control.FeedForwardSettings;
+import com.spikes2212.control.PIDSettings;
+import com.spikes2212.dashboard.RootNamespace;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+
+import java.util.function.Supplier;
+
+public class RotateWithPID extends Command {
+
+    private static final RootNamespace namespace = new RootNamespace("rotate with pid");
+    private static final PIDSettings pidSettings = namespace.addPIDNamespace("rotate with pid");
+    private static final FeedForwardSettings feedForwardSettings = namespace.addFeedForwardNamespace("rotate with pid",
+            FeedForwardSettings.EMPTY_FF_SETTINGS);
+
+    private final Drivetrain drivetrain;
+    private final Supplier<Double> setpoint;
+    private final PIDController pidController;
+    private final FeedForwardController feedForwardController;
+    private double lastTimeOnTarget;
+
+    public RotateWithPID(Drivetrain drivetrain, Supplier<Double> setpoint) {
+        this.drivetrain = drivetrain;
+        this.setpoint = setpoint;
+        this.pidController = new PIDController(pidSettings.getkP(), pidSettings.getkI(), pidSettings.getkD());
+        pidController.setTolerance(pidSettings.getTolerance());
+        this.feedForwardController = new FeedForwardController(feedForwardSettings.getkS(),
+                feedForwardSettings.getkV(), feedForwardSettings.getControlMode());
+    }
+
+    @Override
+    public void execute() {
+        pidController.setPID(pidSettings.getkP(), pidSettings.getkI(), pidSettings.getkD());
+        pidController.setTolerance(pidSettings.getTolerance());
+        feedForwardController.setGains(feedForwardSettings);
+        drivetrain.drive(0, 0, pidController.calculate(drivetrain.getYaw(), setpoint.get()) +
+                feedForwardController.calculate(drivetrain.getYaw(), setpoint.get()),
+                false, false);
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (!pidController.atSetpoint()) {
+            lastTimeOnTarget = Timer.getFPGATimestamp();
+        }
+        return pidSettings.getWaitTime() <= Timer.getFPGATimestamp() - lastTimeOnTarget;
+    }
+}

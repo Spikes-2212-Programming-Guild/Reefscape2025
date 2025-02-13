96284b571c686c34f46f06aff30757db1d3612f6
GIL <eingar.gil@gmail.com>
2025-02-13 16:17:31 +0200

    added PlaceCoralAndTakeAlgae.java

---
+++ src/main/java/frc/robot/commands/PlaceCoralAndTakeAlgae.java
@@ -0,0 +1,31 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.*;
+
+public class PlaceCoralAndTakeAlgae extends SequentialCommandGroup {
+
+    public PlaceCoralAndTakeAlgae(Elevator elevator, AlgaeJoint algaeJoint, Gripper gripper,
+                                  Drivetrain drivetrain, CoralJoint coralJoint, Storage storage,
+                                  Elevator.ElevatorLevel level) {
+        addCommands(new ParallelCommandGroup(
+                        new MoveToHeight(elevator, level),
+                        new RotateStorage(coralJoint, CoralJoint.StoragePose.PLACEMENT),
+                        new RotateAlgaeJointToBottom(algaeJoint)),
+                new ParallelCommandGroup(
+                        new ReleaseCoral(storage), new IntakeAlgae(gripper)),
+                new ConditionalCommand(new SequentialCommandGroup(new ParallelCommandGroup(
+                        new Drive(drivetrain, () -> -0.2, () -> 0.0, () -> 0.0, false,
+                                false, false),
+                        new RotateStorage(coralJoint, CoralJoint.StoragePose.RESTING)),
+                        new MoveToHeight(elevator, Elevator.ElevatorLevel.PROCESSOR)),
+                        new ParallelCommandGroup(
+                                new RotateAlgaeJointToTop(algaeJoint),
+                                new RotateStorage(coralJoint, CoralJoint.StoragePose.INTAKE),
+                                new MoveToHeight(elevator, Elevator.ElevatorLevel.BOTTOM)
+                        ), gripper::hasAlgae));
+
+    }
+}

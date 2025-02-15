9c81509e220e8f6433781980dfedf74a3a550ec1
GIL <eingar.gil@gmail.com>
2025-02-15 10:55:29 +0200

    extends drive

--- src/main/java/frc/robot/commands/autonomous/DriveAuto.java
+++ src/main/java/frc/robot/commands/autonomous/DriveAuto.java
@@ -1,33 +1,22 @@
 package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.Drive;
 import frc.robot.subsystems.Drivetrain;
 
-public class DriveAuto extends Command {
+import java.util.function.Supplier;
 
-    private static final double DRIVE_SPEED = 0.5;
+public class DriveAuto extends Drive {
+
+    private static final Supplier<Double> DRIVE_SPEED = () -> 0.5;
     private static final double DRIVE_TIME = 3;
 
-    private final Drivetrain drivetrain;
-
     public DriveAuto(Drivetrain drivetrain) {
-        addRequirements(drivetrain);
-        this.drivetrain = drivetrain;
-    }
-
-    @Override
-    public void execute() {
-        drivetrain.drive(DRIVE_SPEED, 0.0, 0.0, false, false);
+        super(drivetrain, DRIVE_SPEED, () -> 0.0, () -> 0.0, false, false, false);
     }
 
     @Override
     public boolean isFinished() {
         return DRIVE_TIME >= Timer.getFPGATimestamp();
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        drivetrain.stop();
     }
 }

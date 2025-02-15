5064429fe0f5ba14472235d7c29803a406b1ec7b
GIL <eingar.gil@gmail.com>
2025-02-15 02:00:33 +0200

    added DriveAuto.java

---
+++ src/main/java/frc/robot/commands/autonomous/DriveAuto.java
@@ -0,0 +1,33 @@
+package frc.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+
+public class DriveAuto extends Command {
+
+    private static final double DRIVE_SPEED = 0.5;
+    private static final double DRIVE_TIME = 3;
+
+    private final Drivetrain drivetrain;
+
+    public DriveAuto(Drivetrain drivetrain) {
+        addRequirements(drivetrain);
+        this.drivetrain = drivetrain;
+    }
+
+    @Override
+    public void execute() {
+        drivetrain.drive(0.0, DRIVE_SPEED, 0.0, false, false);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return DRIVE_TIME >= Timer.getFPGATimestamp();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.stop();added
+    }
+}

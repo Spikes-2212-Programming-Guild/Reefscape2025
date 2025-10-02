ad5998da950a649d4915836e8b03cbfaaa86bd69
GIL <eingar.gil@gmail.com>
2025-10-02 23:42:14 +0300

    changed all ports to be neutral

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,38 +4,38 @@ public class RobotMap {
 
     public interface CAN {
 
-        int FRONT_LEFT_DRIVE_SPARK_MAX= 1;
-        int FRONT_RIGHT_DRIVE_SPARK_MAX = 4;
-        int BACK_LEFT_DRIVE_SPARK_MAX = 7;
-        int BACK_RIGHT_DRIVE_SPARK_MAX = 10;
+        int FRONT_LEFT_DRIVE_SPARK_MAX= -1;
+        int FRONT_RIGHT_DRIVE_SPARK_MAX = -1;
+        int BACK_LEFT_DRIVE_SPARK_MAX = -1;
+        int BACK_RIGHT_DRIVE_SPARK_MAX = -1;
 
-        int FRONT_LEFT_TURN_SPARK_MAX = 2;
-        int FRONT_RIGHT_TURN_SPARK_MAX = 5;
-        int BACK_LEFT_TURN_SPARK_MAX = 8;
-        int BACK_RIGHT_TURN_SPARK_MAX = 11;
+        int FRONT_LEFT_TURN_SPARK_MAX = -1;
+        int FRONT_RIGHT_TURN_SPARK_MAX = -1;
+        int BACK_LEFT_TURN_SPARK_MAX = -1;
+        int BACK_RIGHT_TURN_SPARK_MAX = -1;
 
-        int FRONT_LEFT_ABSOLUTE_ENCODER = 3;
-        int FRONT_RIGHT_ABSOLUTE_ENCODER = 6;
-        int BACK_LEFT_ABSOLUTE_ENCODER = 9;
-        int BACK_RIGHT_ABSOLUTE_ENCODER = 12;
+        int FRONT_LEFT_ABSOLUTE_ENCODER = -1;
+        int FRONT_RIGHT_ABSOLUTE_ENCODER = -1;
+        int BACK_LEFT_ABSOLUTE_ENCODER = -1;
+        int BACK_RIGHT_ABSOLUTE_ENCODER = -1;
 
         int ELEVATOR_MASTER_SPARK = -1;
         int ELEVATOR_SLAVE_SPARK = -1;
-        int ALGAE_JOINT_SPARK = 13;
-        int CORAL_JOINT_TALON = 16;
-        int GRIPPER_VICTOR = 17;
-        int STORAGE_SPARK = 14;
+        int ALGAE_JOINT_SPARK = -1;
+        int CORAL_JOINT_TALON = -1;
+        int GRIPPER_VICTOR = -1;
+        int STORAGE_SPARK = -1;
     }
 
     public interface DIO {
 
         int ELEVATOR_TOP_LIMIT = -1;
         int ELEVATOR_BOTTOM_LIMIT = -1;
-        int ALGAE_TOP_LIMIT = 0;
+        int ALGAE_TOP_LIMIT = -1;
         int CORAL_JOINT_TOP_LIMIT = -1;
-        int CORAL_JOINT_BOTTOM_LIMIT = 8;
-        int GRIPPER_LIMIT = 5;
-        int STORAGE_INFRARED = 3;
+        int CORAL_JOINT_BOTTOM_LIMIT = -1;
+        int GRIPPER_LIMIT = -1;
+        int STORAGE_INFRARED = -1;
     }
 
     public interface PWM {

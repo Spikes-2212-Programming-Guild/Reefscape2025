da2be18a7c9de70dbc4e2185d2e81b2e40615432
GIL <eingar.gil@gmail.com>
2025-10-02 23:40:44 +0300

    changed motor types from talonfx and spark max to spark max and spark max

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,10 +4,10 @@ public class RobotMap {
 
     public interface CAN {
 
-        int FRONT_LEFT_DRIVE_TALON_FX = 1;
-        int FRONT_RIGHT_DRIVE_TALON_FX = 4;
-        int BACK_LEFT_DRIVE_TALON_FX = 7;
-        int BACK_RIGHT_DRIVE_TALON_FX = 10;
+        int FRONT_LEFT_DRIVE_SPARK_MAX= 1;
+        int FRONT_RIGHT_DRIVE_SPARK_MAX = 4;
+        int BACK_LEFT_DRIVE_SPARK_MAX = 7;
+        int BACK_RIGHT_DRIVE_SPARK_MAX = 10;
 
         int FRONT_LEFT_TURN_SPARK_MAX = 2;
         int FRONT_RIGHT_TURN_SPARK_MAX = 5;

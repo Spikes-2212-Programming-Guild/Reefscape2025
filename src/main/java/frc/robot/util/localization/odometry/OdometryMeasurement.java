a2410fdd4d9f9bd1cc235ff01bdc5502969dc436
itay <itay2318@gmail.com>
2025-11-08 12:21:00 +0200

    Added localization? I'm Sorry

--- src/main/java/frc/robot/util/localization/odometry/OdometryMeasurement.java
+++ src/main/java/frc/robot/util/localization/odometry/OdometryMeasurement.java
@@ -7,7 +7,7 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
  * Represents a single odometry measure.
  *
  * @param timestamp      measurement timestamp
- * @param heading   gyro heading at the timestamp
+ * @param heading        gyro heading at the timestamp
  * @param wheelPositions swerve module wheel rotations & positions at the timestamp
  * @author Itay Zadok
  */
